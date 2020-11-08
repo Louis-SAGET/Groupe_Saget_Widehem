94e0cc9a9c7d15b21381e4ec689f50b4e776eaa4
Louis SAGET <louis.saget1@etu.univ-lorraine.fr>
2020-11-08 15:35:43 +0100

    Modifications

--- src/Database/ConnectionSingleton.java
+++ src/Database/ConnectionSingleton.java
@@ -1,39 +1,38 @@
 package Database;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class ConnectionSingleton {
-    private Connection c;
-    private static ConnectionSingleton s;
+	private Connection c;
+	private static ConnectionSingleton s;
 
-    private ConnectionSingleton(){
-        try{
-            String url = "jdbc:oracle:thin:@localhost:1521:XE";
-            c= DriverManager.getConnection(url,"chiappin3u", "CiaoBello54400");
-        }catch(SQLException e){
-            System.out.println(e);
-        }
-    }
+	private ConnectionSingleton() {
+		try {
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+			c = DriverManager.getConnection(url, "...", "...");
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
 
+	public static ConnectionSingleton getInstance() {
+		if (s == null) {
+			s = new ConnectionSingleton();
+		} else {
+			try {
+				if (s.c != null && s.c.isClosed()) {
+					s = new ConnectionSingleton();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return s;
+	}
 
-    public static ConnectionSingleton getInstance(){
-        if(s == null){
-            s = new ConnectionSingleton();
-        }else {
-            try {
-                if(s.c!=null && s.c.isClosed()){
-                    s = new ConnectionSingleton();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return s;
-    }
-
-    public Connection getConnection(){
-        return c;
-    }
+	public Connection getConnection() {
+		return c;
+	}
 }
