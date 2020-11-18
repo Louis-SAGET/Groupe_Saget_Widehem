a097080adb2ca80f236930424821597a29a6f0a5
Louis SAGET <louis.saget1@etu.univ-lorraine.fr>
2020-11-18 14:47:20 +0100

    Modification Singleton

--- src/Database/ConnectionSingleton.java
+++ src/Database/ConnectionSingleton.java
@@ -5,11 +5,14 @@ import java.sql.*;
 public class ConnectionSingleton {
 	private Connection c;
 	private static ConnectionSingleton s;
+	
+	private StringBuffer login;
+	private StringBuffer password;
 
 	private ConnectionSingleton() {
 		try {
 			String url = "jdbc:oracle:thin:@localhost:1521:XE";
-			c = DriverManager.getConnection(url, "...", "...");
+			c = DriverManager.getConnection(url, new String(this.login), new String(this.password));
 		} catch (SQLException e) {
 			System.err.println("SQLException: " + e.getMessage());
 		}
@@ -29,10 +32,32 @@ public class ConnectionSingleton {
 		}
 		return s;
 	}
+	
+	public void open(String user, String mdp) {
+		try {
+			if (s.c.isClosed()) {
+				s.login = new StringBuffer(user);
+				s.login = new StringBuffer(mdp);
+				ConnectionSingleton.getInstance();
+			} else {
+				s.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("SQLException: " + e.getMessage());
+		}
+		
+	}
 
 	public void close() {
 		try {
-			this.c.close();
+			if (s.login.length() > 0 && s.password.length() > 0) {
+				this.login.delete(0, this.login.length() - 1);
+				this.password.delete(0, this.password.length() - 1);
+			}			
+			
+			if (!s.c.isClosed()) {
+				s.c.close();
+			}			
 		} catch (SQLException e) {
 			System.err.println("SQLException: " + e.getMessage());
 		}
